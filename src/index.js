70cf510433fc0e6cb4e20c1550ab184c3deef462
bahri-gf <bahri@gamefactory.gs>
2022-02-07 15:06:19 +0300

    init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import WebFont from "webfontloader";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import "./index.css";
 import "./style.css";
 
 class App extends Component {
@@ -34,6 +35,7 @@ class App extends Component {
       this.drawImage();
     });
   }
+
   componentDidMount() {
     WebFont.load({
       google: {
@@ -47,9 +49,35 @@ class App extends Component {
 
   render() {
     return (
-      <div className="main">
-        <div>
-          <h1>GameFactory Logo Generator</h1>
+      <div className="">
+
+        <nav class="flex bg-white flex-wrap items-center justify-between p-4">
+            <div class="lg:order-2 w-auto lg:w-3/5 lg:text-center">
+                <a class="text-3xl text-black font-semibold font-heading" href="#">
+                    Game Factory Logo Generator
+                </a>
+            </div>
+            <div class="block lg:hidden">
+                <button class="navbar-burger flex items-center py-2 px-3 text-indigo-500 rounded border border-indigo-500">
+                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>
+                            Menu
+                        </title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z">
+                        </path>
+                    </svg>
+                </button>
+            </div>
+            <div class="navbar-menu hidden lg:order-1 lg:block w-full lg:w-1/5">
+
+            </div>
+            <div class="navbar-menu hidden lg:order-3 lg:block w-full lg:w-1/5 lg:text-right">
+
+            </div>
+        </nav>
+
+        <div class="p-8">
+
           <div style={hidden}>
             <img
               ref={e => {
@@ -58,52 +86,133 @@ class App extends Component {
               onLoad={() => {
                 this.drawImage();
               }}
-              src={this.state.colorful ? "black.svg" : "black.svg"}
+              src={this.state.colorful ? "turuncu_125.svg" : "beyaz_125.svg"}
               alt={`DSC Icon`}
             />
           </div>
-          <p>Start editing to see some magic happen :)</p>
-          <div className="controls">
-            {this.renderScaleButton()}
-            <select defaultValue={this.state.structure} onChange={this.handleStructureChange}>
-              <option disabled={true}>Select Style</option>
-              <option value="logo_left">Logo Left</option>
-              <option value="logo_center">Logo Center</option>
-            </select>
-            <select defaultValue={this.state.naming_scheme} ref="naming_scheme" onChange={this.handleChange}>
-              <option disabled={true}>Select Naming Scheme</option>
-              <option value="GFSC">GFSC</option>
-              <option value="GameFactory Student Clubs">GameFactory Student Clubs</option>
-            </select>
-            <select defaultValue={this.state.coloration} onChange={this.handleColorChange}>
-              <option disabled={true}>Select Coloration</option>
-              <option value="Colorful">Colorful</option>
-              <option value="Knockout">Knockout</option>
-            </select>
+
+
+          <div className="controls" class="inline-block bg-white dark:bg-gray-800">
+              <div class="">
+
+                  <ul class="text-lg font-light flex flex-wrap justify-between">
+
+               			<div class="my-8 pr-4">
+                      <div class="flex items-center justify-center space-x-3">
+                        <button class="bg-blue-500 px-4 py-2 font-semibold text-white inline-flex items-center space-x-2 rounded" 
+                          onClick={() =>
+                            this.setState(
+                              {
+                                scale:
+                                  this.state.scale > 1 ? this.state.scale - 1 : this.state.scale
+                              },
+                              () => {
+                                this.drawImage();
+                              }
+                            )
+                          }>
+                          <span>-</span>
+                        </button>
+                        <span>Scale</span>
+                        <button class="bg-red-500 px-4 py-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+                          onClick={() =>
+                            this.setState(
+                              {
+                                scale:
+                                  this.state.scale < 5 ? this.state.scale + 1 : this.state.scale
+                              },
+                              () => {
+                                this.drawImage();
+                              }
+                            )
+                          }>
+                          <span>+</span>
+                        </button>
+                      </div>
+               			</div>
+
+                    <label class="my-2 text-gray-700">
+                        İsim Şeması
+                        <select defaultValue={this.state.naming_scheme} ref="naming_scheme" onChange={this.handleChange} class="block w-52 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500">
+                            <option disabled={true}>
+                                İsim Şeması Seç
+                            </option>
+                            <option value="GFSC">
+                              GFSC
+                            </option>
+                            <option value="Game Factory Student Clubs">
+                              Game Factory Student Clubs
+                            </option>
+                        </select>
+                    </label>
+
+                    <label class="my-2 text-gray-700">
+                        Tema
+                        <select defaultValue={this.state.coloration} onChange={this.handleColorChange} class="block w-52 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500">
+                            <option disabled={true}>
+                                Tema Seç
+                            </option>
+                            <option value="Colorful">
+                                Renkli
+                            </option>
+                            <option value="Knockout">
+                                Siyah-Beyaz
+                            </option>
+                        </select>
+                    </label>
+
+                    <label class="my-2 text-gray-700">
+                        Üniversite
+                        <input 
+                        onChange={e => {
+                          this.setState(
+                            {
+                              name: e.target.value
+                            },
+                            () => {
+                              this.drawImage();
+                            }
+                          );
+                        }} 
+                        type="text" 
+                        id="name-with-label" 
+                        class="block w-52 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" 
+                        name="club" 
+                        placeholder="Kulüp İsmi"/>
+                    </label>
+
+                    <label class="my-9 pl-4">
+                        <Button 
+                        href={this.state.fullLogoUrl}
+                        download={`GFSC ${this.state.name} Logo x${this.state.scale}.png`}
+                        type="button" 
+                        class="py-2 px-4 flex justify-center items-center bg-green-500 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-full">
+                            <svg width="20" height="20" class="mr-2" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1344 1472q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm256 0q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm128-224v320q0 40-28 68t-68 28h-1472q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h427q21 56 70.5 92t110.5 36h256q61 0 110.5-36t70.5-92h427q40 0 68 28t28 68zm-325-648q-17 40-59 40h-256v448q0 26-19 45t-45 19h-256q-26 0-45-19t-19-45v-448h-256q-42 0-59-40-17-39 14-69l448-448q18-19 45-19t45 19l448 448q31 30 14 69z">
+                                </path>
+                            </svg>
+                            KAYDET
+                        </Button>
+                    </label>
+
+                  </ul>
+
+              </div>
           </div>
-          <TextField
-            label="Value right to the logo"
-            margin="normal"
-            onChange={e => {
-              this.setState(
-                {
-                  name: e.target.value
-                },
-                () => {
-                  this.drawImage();
-                }
-              );
-            }}
-          />
+
           <br />
+
           <canvas
             style={hidden}
             ref={e => {
               this.logoCanvas = e;
             }}
-          ></canvas>
+          >
+          </canvas>
+          
           <div className={this.state.colorful ? "full-logo-container" : "full-logo-container dark"}>
             <img
+              className={""}
               ref={e => {
                 this.fullLogoImg = e;
               }}
@@ -111,18 +220,9 @@ class App extends Component {
               src={this.state.fullLogoUrl}
             />
           </div>
-          <Button
-            variant="contained"
-            color="primary"
-            href={this.state.fullLogoUrl}
-            download={`GFSC ${this.state.name} Logo x${this.state.scale}.png`}
-          >
-            SAVE IMAGE
-            </Button>
+
         </div>
-        <footer>
-          Made with by <a href="https://twitter.com/BahriMelih" target="_blank" rel="noopener noreferrer">@melihbahri</a>
-        </footer>
+
       </div>
     );
   }
@@ -134,7 +234,7 @@ class App extends Component {
     const colorful = this.state.colorful;
     const scale = this.state.scale;
     const ctx = this.logoCanvas.getContext("2d");
-    ctx.font = `94px "Product Sans"`;
+    ctx.font = `72px "Reef"`;
 
     // measures the minimum width of the text to ensure the canvas is expanded at least by this
     // value.
@@ -151,10 +251,16 @@ class App extends Component {
       this.logoCanvas.setAttribute("height", (this.dscLogo.height * 2 * scale) + 50);
 
     ctx.scale(scale, scale);
-    ctx.font = `94px "Product Sans"`;
+    ctx.font = `72px "Reef"`;
     ctx.fillStyle = colorful ? "rgb(103, 108, 114)" : "rgb(255, 255, 255)";
-    let imageDX = structure === "logo_center" ? (minimumTextWidth / 2 - (this.dscLogo.width / 2)) : 0;
-    ctx.drawImage(this.dscLogo, imageDX, 0, this.dscLogo.width, this.dscLogo.height);
+
+    if (naming_scheme === "GFSC") {
+      let imageDX = structure === "logo_center" ? (minimumTextWidth / 2 - (this.dscLogo.width / 2)) : 0;
+      ctx.drawImage(this.dscLogo, imageDX, 0, this.dscLogo.width, this.dscLogo.height);
+    } else {
+      let imageDX = structure === "logo_center" ? (minimumTextWidth / 2 - (this.dscLogo.width / 2)) : 20;
+      ctx.drawImage(this.dscLogo, imageDX, 70, this.dscLogo.width, this.dscLogo.height);
+    }
 
     //if logo center
     if (structure === "logo_center") {
@@ -164,7 +270,7 @@ class App extends Component {
     } else {
       //if GFSC is selected, draw on one line
       if (naming_scheme === "GFSC") {
-        ctx.fillText(naming_scheme + " " + name, this.dscLogo.width + 40, 110);
+        ctx.fillText(naming_scheme + " " + name, this.dscLogo.width + 20, 90);
       } else {
         ctx.fillText(naming_scheme, this.dscLogo.width + 40, 110);
         ctx.fillText(name, this.dscLogo.width + 40, 200);
@@ -195,7 +301,7 @@ class App extends Component {
         >
           -
         </button>
-        <span>Scale</span>
+        <span>Boyut</span>
         <button
           onClick={() =>
             this.setState(
